29b174f198c511805b11195de426fda51fc048f0
Guga-CL <gustavo.clima@yahoo.com.br>
2025-11-14 16:46:23 -0300

    more stuff

--- src/My.Custom.Section.Plugin/Plugin.cs
+++ src/My.Custom.Section.Plugin/Plugin.cs
@@ -2,19 +2,26 @@
 using MediaBrowser.Common.Plugins;
 using MediaBrowser.Model.Plugins;
 using MediaBrowser.Model.Serialization;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Loader;
 
 namespace My.Custom.Section.Plugin
 {
     public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
     {
         public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer)
-            : base(applicationPaths, xmlSerializer) { }
+            : base(applicationPaths, xmlSerializer)
+        {
+            RegisterHomeScreenSection();
+        }
 
         public override string Name => "My Custom Section";
         public override string Description => "Minimal test plugin to register a Home Screen Section.";
-        public override Guid Id => Guid.Parse("9194a4db-c196-44ab-bf6a-b6c54da9414a");
+        public override Guid Id => Guid.Parse("aef0c16b-7e00-456c-b4df-0dc38c42e942");
 
         public IEnumerable<PluginPageInfo> GetPages()
         {
@@ -27,5 +34,45 @@ namespace My.Custom.Section.Plugin
                 }
             };
         }
+
+        private void RegisterHomeScreenSection()
+        {
+            var payload = new JObject
+            {
+                ["Section"] = "MyCustomSection",
+                ["id"] = Guid.NewGuid().ToString(),
+                ["displayText"] = "My Custom Section",
+                ["limit"] = 1,
+                ["route"] = "mycustomsection",
+                ["additionalData"] = "extra-info",
+                ["resultsAssembly"] = GetType().Assembly.FullName,
+                ["resultsClass"] = typeof(SectionResults).FullName,
+                ["resultsMethod"] = nameof(SectionResults.GetResults),
+
+                // Add this block so the section appears in /HomeScreen/Sections
+                ["OriginalPayload"] = new JObject
+                {
+                    ["Name"] = "My Custom Section",
+                    ["ServerId"] = Guid.NewGuid().ToString(),
+                    ["Id"] = Guid.NewGuid().ToString(),
+                    ["Type"] = "Folder",
+                    ["Overview"] = "Dummy section from plugin",
+                    ["CollectionType"] = "animes"
+                }
+            };
+
+            var homeScreenSectionsAssembly = AssemblyLoadContext.All
+                .SelectMany(x => x.Assemblies)
+                .FirstOrDefault(x => x.FullName?.Contains(".HomeScreenSections") ?? false);
+
+            if (homeScreenSectionsAssembly != null)
+            {
+                var pluginInterfaceType = homeScreenSectionsAssembly.GetType("Jellyfin.Plugin.HomeScreenSections.PluginInterface");
+                pluginInterfaceType?.GetMethod("RegisterSection")
+                    ?.Invoke(null, new object?[] { payload });
+            }
+        }
+
+
     }
 }
---
+++ src/My.Custom.Section.Plugin/SectionResults.cs
@@ -0,0 +1,40 @@
+using MediaBrowser.Model.Querying;
+using MediaBrowser.Model.Dto;
+using Jellyfin.Data.Enums;
+using MediaBrowser.Model.Entities; 
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+
+namespace My.Custom.Section.Plugin
+{
+    public static class SectionResults
+    {
+        // Must accept JObject, not object
+        public static QueryResult<BaseItemDto> GetResults(JObject request)
+        {
+            var items = new List<BaseItemDto>
+            {
+                new BaseItemDto
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Hello Jellyfin!",
+                    Type = BaseItemKind.Movie,
+                    Overview = "This is a dummy item from My Custom Section plugin.",
+                    ProductionYear = 2025,
+                    PremiereDate = DateTime.UtcNow,
+                    ImageTags = new Dictionary<ImageType,string>
+                    {
+                        { ImageType.Primary, Guid.NewGuid().ToString() }
+                    }
+                }
+            };
+
+            return new QueryResult<BaseItemDto>
+            {
+                Items = items,
+                TotalRecordCount = items.Count
+            };
+        }
+    }
+}
